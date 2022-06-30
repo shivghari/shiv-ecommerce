9d6741f3e3cf5d43df266e56a791c3c72638829d
shivghari <shiv.i.ghariwala@gmail.com>
2022-06-30 11:14:48 +0530

    Add AddToCart and AddToWish To Latest Product Section on Home Page

--- ecom/src/Component/LatestProductSec/LatestProduct.jsx
+++ ecom/src/Component/LatestProductSec/LatestProduct.jsx
@@ -1,52 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LatestProduct.css";
-import { Nav, Tab } from "react-bootstrap";
-import NewArrival from "./NewArrival/NewArrival";
-import BestSeller from "./BestSeller/BestSeller";
-import FeaturedList from "./FeaturedList/FeaturedList";
-import SpecialOffer from "./SpecialOffer/SpecialOffer";
+// import { Col, Nav, Row, Tab } from "react-bootstrap";
+// import NewArrival from "./NewArrival/NewArrival";
+// import BestSeller from "./BestSeller/BestSeller";
+// import FeaturedList from "./FeaturedList/FeaturedList";
+// import SpecialOffer from "./SpecialOffer/SpecialOffer";
+import SingleProductDesign from "./SingleProductDesign/SingleProductDesign";
+import axios from "axios";
 
 function LatestProduct() {
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/homepageproduct/latestgetAllProducts")
+      .then((response) => {
+        setdata(response.data.response);
+        console.log(response.data.response);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <div className="latestProductSec">
       <div className="LatestProductHeading">
         <h1>Latest product</h1>
       </div>
       <div className="productContainer">
-        <Tab.Container defaultActiveKey={"newArrival"}>
-          <Nav>
-            <div className="latestProductLinks">
-              <Nav.Item>
-                <Nav.Link eventKey={"newArrival"} style={{ color: "#151875" }}>
-                  New Arrival
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey={"bestSeller"}>Best Seller</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey={"featured"}>Featured</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey={"specialOffer"}>Special Offer</Nav.Link>
-              </Nav.Item>
-            </div>
-          </Nav>
-          <Tab.Content>
-            <Tab.Pane eventKey={"newArrival"}>
-              <NewArrival />
-            </Tab.Pane>
-            <Tab.Pane eventKey={"bestSeller"}>
-              <BestSeller />
-            </Tab.Pane>
-            <Tab.Pane eventKey={"featured"}>
-              <FeaturedList />
-            </Tab.Pane>
-            <Tab.Pane eventKey={"specialOffer"}>
-              <SpecialOffer />
-            </Tab.Pane>
-          </Tab.Content>
-        </Tab.Container>
+        {data.map((item) => (
+          <SingleProductDesign
+            productImage={`http://localhost:3001/static/${item.image}`}
+            productName={`${item.prouctname}`}
+            productPrice={`$${item.price}`}
+            strikedPrice={`$${item.stakedprice}`}
+            rating={item.rating}
+            productCode={item._id}
+          />
+        ))}
       </div>
     </div>
   );
--- ecom/src/Component/LatestProductSec/SingleProductDesign/SingleProductDesign.jsx
+++ ecom/src/Component/LatestProductSec/SingleProductDesign/SingleProductDesign.jsx
@@ -1,19 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SingleProductDesign.css";
 
 // import Card from "@mui/material/Card";
 import { Card } from "antd";
+import { Rating } from "@mui/material";
 
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ZoomInOutlinedIcon from "@mui/icons-material/ZoomInOutlined";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useDispatch } from "react-redux";
+import { addItem } from "../../../Feature/cartSlice";
+import axios from "axios";
 
 function SingleProductDesign({
   productImage,
   productName,
   productPrice,
   strikedPrice,
+  rating,
+  productCode,
 }) {
+  const [liked, setliked] = useState();
+  const [unlike, setunlike] = useState(true);
+  const [check, setCheck] = useState();
+
+  const dispatch = useDispatch();
+
+  const addToCart = (price) => {
+    axios
+      .post("http://localhost:3001/productPage/addToCart", {
+        userID: JSON.parse(localStorage.getItem("token")).userID,
+        productID: productCode,
+        productPrice: productPrice,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const addATowish = (productID) => {
+    axios
+      .post("http://localhost:3001/productPage/addTowish", {
+        userID: JSON.parse(localStorage.getItem("token")).userID,
+        productID: productCode,
+      })
+      .then((response) => {
+        console.log("data", response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const removeFromWish = (productID) => {
+    axios
+      .post("http://localhost:3001/productPage/removeFromwish", {
+        userID: JSON.parse(localStorage.getItem("token")).userID,
+        productID: productCode,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err, "err");
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/productPage/getWishListUser", {
+        userID: JSON.parse(localStorage.getItem("token")).userID,
+      })
+      .then((response) => {
+        if (response.data.wishlist.includes(productCode)) {
+          setliked(true);
+          setCheck(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="SingleproductCardContainer">
       <Card style={{ width: "300px" }}>
@@ -28,15 +101,49 @@ function SingleProductDesign({
                     marginBottom: "10px",
                     cursor: "pointer",
                   }}
-                />
-                <FavoriteBorderOutlinedIcon
-                  sx={{
-                    fontSize: "18px",
-                    color: "#1389FF",
-                    marginBottom: "10px",
-                    cursor: "pointer",
+                  onClick={() => {
+                    dispatch(
+                      addItem({
+                        productID: productCode,
+                        price: parseInt(productPrice),
+                        total: 1,
+                      })
+                    );
+                    addToCart(productPrice);
                   }}
                 />
+                {unlike && !check ? (
+                  <FavoriteBorderOutlinedIcon
+                    sx={{
+                      fontSize: "18px",
+                      color: "#1389FF",
+                      cursor: "pointer",
+                      marginBottom: "10px",
+                    }}
+                    onClick={() => {
+                      addATowish(productCode);
+                      setliked(true);
+                      setunlike(false);
+                      setCheck(true);
+                    }}
+                  />
+                ) : liked ? (
+                  <FavoriteIcon
+                    sx={{
+                      fontSize: "18px",
+                      color: "#1389FF",
+                      cursor: "pointer",
+                      marginBottom: "10px",
+                    }}
+                    onClick={() => {
+                      removeFromWish(productCode);
+                      setliked(false);
+                      setunlike(true);
+                      setCheck(false);
+                    }}
+                  />
+                ) : null}
+
                 <ZoomInOutlinedIcon
                   sx={{
                     fontSize: "18px",
@@ -48,9 +155,15 @@ function SingleProductDesign({
               </div>
             </div>
             <div className="imgComtainer">
-              <img src={productImage} alt="New Arrival" />
+              <img
+                src={productImage}
+                alt="New Arrival"
+                height="200px"
+                width="200px"
+              />
             </div>
           </div>
+          <Rating defaultValue={rating} precision={1} readOnly />
           <div className="productDesc">
             <p className="arrivalProductName">{productName}</p>
             <p className="arrivalProductPrice">
